1f6a5d78c386dad9e6a85e7258339df16cf32cff
hadabas <hadabas12@hotmail.com>
2019-01-25 13:19:45 +0100

    update

--- extra/docker/mongo_docker/help_script/helper.sh
+++ extra/docker/mongo_docker/help_script/helper.sh
@@ -6,3 +6,8 @@ mongo SZBK-felhasznalok \
         --host localhost \
         --port 27017 \
         --eval "db.createCollection('Felhasznalok');"
+
+mongo SZBK-felhasznalok \
+        --host localhost \
+        --port 27017 \
+        --eval 'db.Felhasznalok.insertOne({"username": "${DEFAULT_USERNAME}","password": "${DEFAULT_PASSWORD}", "role": "admin"})'
