b0114d43a9a212c434fe8b5fe5defcf479f11c09
hadabas <hadabas12@hotmail.com>
2019-02-24 11:52:16 +0100

    fix

--- extra/docker/mongo_docker/help_script/helper.sh
+++ extra/docker/mongo_docker/help_script/helper.sh
@@ -11,3 +11,18 @@ mongo SZBK-felhasznalok \
         --host localhost \
         --port 27017 \
         --eval "db.Felhasznalok.insertOne({'username': '${DEFAULT_USERNAME}','password': '${DEFAULT_PASSWORD}', 'role': 'admin'})"
+
+mongo SZBK-raspik \
+        --host localhost \
+        --port 27017 \
+        --eval "db.createCollection('Projects');"
+
+mongo SZBK-raspik \
+        --host localhost \
+        --port 27017 \
+        --eval "db.createCollection('Devices');"
+
+mongo SZBK-adatok \
+        --host localhost \
+        --port 27017 \
+        --eval "db.createCollection('Adatok');"
